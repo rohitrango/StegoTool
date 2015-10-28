83a8a9f0ea16b314540b748ef697b8abb79a5ab9
Rohit Kumar Jena <rohitrango@users.noreply.github.com>
2015-10-28 12:47:19 +0530

    Update bitmap.cpp
    Added support for both 24bpp and 32 bpp bmp images

--- bitmap.cpp
+++ bitmap.cpp
@@ -14,18 +14,15 @@ BMPimage::BMPimage(char* filename)
     // extract image height and width from header
     width = *(int*)&info[18];
     height = *(int*)&info[22];
+    bitDepth = *(int*)&info[28];
+    channels = bitDepth/8;
 
-    size = 3 * width * height;
+    size = *(int*)&info[34];
     data = new unsigned char[size]; // allocate 3 bytes per pixel
     fread(data, sizeof(unsigned char), size, f); // read the rest of the data at once
     fclose(f);
 
-    for(unsigned int i = 0; i < size; i += 3)
-    {
-            unsigned char tmp = data[i];
-            data[i] = data[i+2];
-            data[i+2] = tmp;
-    }
+
 }
 
 void BMPimage::writeBMP(char *filename)
@@ -35,12 +32,7 @@ void BMPimage::writeBMP(char *filename)
 
    // unsigned char *newdata = new unsigned char[size];
    // newdata = data;
-    for(unsigned int i = 0; i < size; i += 3)
-    {
-            unsigned char tmp = data[i];
-            data[i] = data[i+2];
-            data[i+2] = tmp;
-    }
+
     fwrite(info, sizeof(unsigned char), 54, f);
     fwrite(data, sizeof(unsigned char), size, f);
     fclose(f);
@@ -64,17 +56,40 @@ const int BMPimage::getHEIGHT()
 
 pixel BMPimage::getPIXEL( int x, int y)
 {
-    int index = (y*width+x)*3;
-    pixel p = {data[index],data[index+1],data[index+2]};
+    int index = (y*width+x)*channels;
+    pixel p;
+    if(channels==3)
+    {
+        p.b = data[index];
+        p.g = data[index+1];
+        p.r = data[index+2];
+    }
+    else if(channels==4)
+    {
+        p.b = data[index];
+        p.g = data[index+1];
+        p.r = data[index+2];
+        p.a = data[index+3];
+    }
     return p;
 }
 
 void BMPimage::changePIXEL( pixel p, int x, int y)
 {
-    int index = (y*width+x)*3;
-    data[index] = p.r;
-    data[index+1] = p.g;
-    data[index+2] = p.b;
+    int index = (y*width+x)*channels;
+    if(channels==3)
+    {
+        data[index] = p.b;
+        data[index+1] = p.g;
+        data[index+2] = p.r;
+    }
+    else if(channels==4)
+    {
+        data[index] = p.b;
+        data[index+1] = p.g;
+        data[index+2] = p.r;
+        data[index+3] = p.a;
+    }
 }
 
 void BMPimage::convertImage()
@@ -85,7 +100,7 @@ void BMPimage::convertImage()
 }
 
 
-void BMPimage::encode(char *encFile)
+void BMPimage::encode(char *encFile)     //just a test algorithm to hide files
 {
     convertImage();
     fstream f(encFile);
@@ -114,7 +129,7 @@ void BMPimage::encode(char *encFile)
     f.close();
 }
 
-void BMPimage::decode()
+void BMPimage::decode()             //test algorithm to decode files
 {
     char save[100],msg;
     cout<<"Save decoded message as...";
@@ -137,4 +152,9 @@ void BMPimage::decode()
     }
     saveFile.close();
 
+}
+
+int BMPimage::getBitDepth()
+{
+        return bitDepth;
 }
